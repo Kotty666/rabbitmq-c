1b02958b6fb10f807619eafba1a72ef6530c47eb
Alan Antonuk <alan.antonuk@gmail.com>
2015-12-24 14:06:20 -0800

    CI: use newer version of cmake on travis.

--- travis.sh
+++ travis.sh
@@ -8,6 +8,7 @@ build_autotools() {
 
 build_cmake() {
   mkdir $PWD/_build && cd $PWD/_build
+  cmake --version
   cmake .. -GNinja -DCMAKE_INSTALL_PREFIX=$PWD/../_install -DCMAKE_C_FLAGS="-Werror"
   cmake --build . --target install
   ctest -V .
@@ -15,6 +16,7 @@ build_cmake() {
 
 build_asan() {
   mkdir $PWD/_build && cd $PWD/_build
+  cmake --version
   cmake .. -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$PWD/../_install \
     -DCMAKE_C_FLAGS="-Werror -fsanitize=address,undefined -O1"
   cmake --build . --target install
@@ -23,6 +25,7 @@ build_asan() {
 
 build_tsan() {
   mkdir $PWD/_build && cd $PWD/_build
+  cmake --version
   cmake .. -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$PWD/../_install \
     -DCMAKE_C_FLAGS="-Werror -fsanitize=thread,undefined -O1"
   cmake --build . --target install
